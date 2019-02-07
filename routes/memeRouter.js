faa5048010735d48d4f5f67c2bba2d3d7adb5f8c
ehiggins98 <ericdhiggins@gmail.com>
2019-02-07 01:12:08 -0600

    Added font endpoint

--- routes/memeRouter.js
+++ routes/memeRouter.js
@@ -2,8 +2,10 @@ const express = require('express')
 const memeManager = require('../memeManager')
 var router = express.Router()
 
-router.get('/', async function(req, res, next) {
-    res.sendFile(await memeManager.getMeme())
+memeManager.init()
+
+router.all('/random', async function(req, res, next) {
+    res.send(memeManager.getMeme())
 })
 
 module.exports = router
